874024de1565c05862622ff4e8aa70f6f6d0c15b
Diego <dfloresgonz@gmail.com>
2025-02-05 22:42:23 -0800

    final clase3 reto

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {  CurrencyPipe, DatePipe, LowerCasePipe, NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, UpperCasePipe } from '@angular/common';
+import {  CurrencyPipe, DatePipe, LowerCasePipe, NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, PercentPipe, UpperCasePipe } from '@angular/common';
 import { Component, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'alrevezPipe',
   standalone: true,
 })
-class MiPipe implements PipeTransform {
+export class MiPipe implements PipeTransform {
   transform(value: string) {
     return value.split('').reverse().join('');
   }
@@ -15,18 +15,27 @@ class MiPipe implements PipeTransform {
   name: 'ofucar',
   standalone: true,
 })
-class OfuscarPipe implements PipeTransform {
+export class OfuscarPipe implements PipeTransform {
   transform(value: string) {
     return `${value.substring(0,3)}***`
   }
 }
 
+type Product = {
+  name: string;
+  price: number;
+  imgUrl: string;
+  description: string;
+  isOferta: boolean;
+  porcentajeOferta: number;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
+  imports: [NgFor,
     UpperCasePipe,LowerCasePipe,DatePipe,CurrencyPipe,
-    MiPipe,OfuscarPipe],
+    MiPipe,OfuscarPipe,PercentPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -40,4 +49,24 @@ export class AppComponent {
   userRole: string = 'admin';
   today: Date = new Date();
   sueldo: number = 1234234289.35563;
+  porcentaje: number = 0.56789;
+
+  productos: Product[] = [
+    {
+      name: 'Laptop',
+      price: 12000,
+      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_826747-MLU77986349091_072024-O.webp`,
+      description: 'Laptop gamer.....',
+      isOferta: false,
+      porcentajeOferta: 0.5,
+    },
+    {
+      name: 'Audifonos',
+      price: 100,
+      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_826747-MLU77986349091_072024-O.webp`,
+      description: 'audifonos...',
+      isOferta: true,
+      porcentajeOferta: 0.25,
+    }
+  ]
 }
