0a0a108ea284467a5ee16d6aaabde70752651a63
Diego <dfloresgonz@gmail.com>
2025-02-05 22:17:30 -0800

    pipes

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,18 +1,43 @@
-import {  NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
-import { Component } from '@angular/core';
+import {  CurrencyPipe, DatePipe, LowerCasePipe, NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, UpperCasePipe } from '@angular/common';
+import { Component, Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'alrevezPipe',
+  standalone: true,
+})
+class MiPipe implements PipeTransform {
+  transform(value: string) {
+    return value.split('').reverse().join('');
+  }
+}
+
+@Pipe({
+  name: 'ofucar',
+  standalone: true,
+})
+class OfuscarPipe implements PipeTransform {
+  transform(value: string) {
+    return `${value.substring(0,3)}***`
+  }
+}
 
 @Component({
   selector: 'app-root',
-  imports: [NgFor,NgIf,NgStyle,NgSwitch,NgSwitchCase,NgSwitchDefault],
+  standalone: true,
+  imports: [
+    UpperCasePipe,LowerCasePipe,DatePipe,CurrencyPipe,
+    MiPipe,OfuscarPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Instagram';
-  nombre: string = "Diego";
+  nombre: string = "dIeGo";
   imagen = `https://robohash.org/${this.nombre}`;
   colorez: string[] = ['rojo', 'verde', 'azul', 'amarillo'];
   hayNotificacion: boolean = true;
   colorBackground: string = 'purple';
-  userRole: string = 'admin'
+  userRole: string = 'admin';
+  today: Date = new Date();
+  sueldo: number = 1234234289.35563;
 }
