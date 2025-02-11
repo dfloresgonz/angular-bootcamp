46f0fdcaff2b860b2c1b9fe58269cc6adec28bd0
Diegol <diegorflores@bcp.com.pe>
2025-02-10 21:07:17 -0500

    feat: home about component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,44 +1,11 @@
-import { Component, Pipe, PipeTransform } from '@angular/core';
-import { ProductosComponent } from './productos/productos.component';
-
-@Pipe({
-  name: 'alrevezPipe',
-  standalone: true,
-})
-export class MiPipe implements PipeTransform {
-  transform(value: string) {
-    return value.split('').reverse().join('');
-  }
-}
-
-@Pipe({
-  name: 'ofucar',
-  standalone: true,
-})
-export class OfuscarPipe implements PipeTransform {
-  transform(value: string) {
-    return `${value.substring(0,3)}***`
-  }
-}
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductosComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Instagram';
-  nombre: string = "dIeGo";
-  imagen = `https://robohash.org/${this.nombre}`;
-  colorez: string[] = ['rojo', 'verde', 'azul', 'amarillo'];
-  hayNotificacion: boolean = true;
-  colorBackground: string = 'purple';
-  userRole: string = 'admin';
-  today: Date = new Date();
-  sueldo: number = 1234234289.35563;
-  porcentaje: number = 0.56789;
-
-  
-}
+export class AppComponent {}
