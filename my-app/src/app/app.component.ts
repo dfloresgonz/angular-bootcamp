b91b2dd540daf8b128ec2de968f78409594277d4
Diego <dfloresgonz@gmail.com>
2025-02-10 16:10:24 -0800

    feat: product component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {  CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { Component, Pipe, PipeTransform } from '@angular/core';
+import { ProductosComponent } from './productos/productos.component';
 
 @Pipe({
   name: 'alrevezPipe',
@@ -21,19 +21,10 @@ export class OfuscarPipe implements PipeTransform {
   }
 }
 
-type Product = {
-  name: string;
-  price: number;
-  imgUrl: string;
-  description: string;
-  isOferta: boolean;
-  porcentajeOferta: number;
-}
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, NgIf, CurrencyPipe],
+  imports: [ProductosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -49,30 +40,5 @@ export class AppComponent {
   sueldo: number = 1234234289.35563;
   porcentaje: number = 0.56789;
 
-  productos: Product[] = [
-    {
-      name: 'Laptop Lenovo V15 G4 Core I5 13420h 16gb 512gb 15.6 Español Color Iron Grey',
-      price: 12300,
-      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_881307-MLU77513227583_072024-O.webp`,
-      description: 'Tarjeta gráfica: Intel UHD Graphics, Con pantalla táctil: No',
-      isOferta: false,
-      porcentajeOferta: 0.5,
-    },
-    {
-      name: 'Audifonos Inalámbricos HUAWEI FreeBuds SE 2 Negro',
-      price: 1500.3455,
-      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_826747-MLU77986349091_072024-O.webp`,
-      description: 'Carga rápida,Cuenta con bluetooth,Resistente al agua.',
-      isOferta: true,
-      porcentajeOferta: 0.12,
-    },
-    {
-      name: 'Sony PlayStation 5 Slim 1tb Digital Color Blanco',
-      price: 17456.97,
-      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_841132-MLU77796368248_072024-O.webp`,
-      description: 'Con tu Consola PlayStation 5 Slim tendrás entretenimiento asegurado todos los días. Su tecnología fue creada para poner nuevos retos tanto a jugadores principiantes como expertos.',
-      isOferta: true,
-      porcentajeOferta: 0.25,
-    }
-  ]
+  
 }
--- my-app/src/app/productos/productos.component.ts
+++ my-app/src/app/productos/productos.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
+type Product = {
+  name: string;
+  price: number;
+  imgUrl: string;
+  description: string;
+  isOferta: boolean;
+  porcentajeOferta: number;
+}
 
 @Component({
-  selector: 'app-productos',
-  imports: [],
+  selector: 'producto-card',
+  imports: [NgFor, NgIf],
   templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent {
-  productName: string = 'Apple macbook pro'
+  productos: Product[] = [
+    {
+      name: 'Laptop Lenovo V15 G4 Core I5 13420h 16gb 512gb 15.6 Español Color Iron Grey',
+      price: 12300,
+      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_881307-MLU77513227583_072024-O.webp`,
+      description: 'Tarjeta gráfica: Intel UHD Graphics, Con pantalla táctil: No',
+      isOferta: false,
+      porcentajeOferta: 0.5,
+    },
+    {
+      name: 'Audifonos Inalámbricos HUAWEI FreeBuds SE 2 Negro',
+      price: 1500.3455,
+      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_826747-MLU77986349091_072024-O.webp`,
+      description: 'Carga rápida,Cuenta con bluetooth,Resistente al agua.',
+      isOferta: true,
+      porcentajeOferta: 0.12,
+    },
+    {
+      name: 'Sony PlayStation 5 Slim 1tb Digital Color Blanco',
+      price: 17456.97,
+      imgUrl: `https://http2.mlstatic.com/D_NQ_NP_841132-MLU77796368248_072024-O.webp`,
+      description: 'Con tu Consola PlayStation 5 Slim tendrás entretenimiento asegurado todos los días. Su tecnología fue creada para poner nuevos retos tanto a jugadores principiantes como expertos.',
+      isOferta: true,
+      porcentajeOferta: 0.25,
+    }
+  ]
 }
