de652594a06c9ce9725280fbff8540d812c241d4
Diegol <diegorflores@bcp.com.pe>
2025-02-12 20:02:28 -0500

    feat: avance clase5

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, MatTabsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  selectedTab: number = 0;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateSelectedTab(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  navigateToTab(event: any) {
+    switch (event.index) {
+      case 0:
+        this.router.navigate(['/']);
+        break;
+      case 1:
+        this.router.navigate(['/about']);
+        break;
+      case 2:
+        this.router.navigate(['/productos']);
+        break;
+    }
+  }
+
+  private updateSelectedTab(url: string) {
+    if (url.includes('/home')) {
+      this.selectedTab = 0;
+    } else if (url.includes('/about')) {
+      this.selectedTab = 1;
+    } else if (url.includes('/productos')) {
+      this.selectedTab = 2;
+    }
+  }
+}
